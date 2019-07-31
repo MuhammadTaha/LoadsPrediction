33cd6f9dbf824825e5df02ad9d03da7d3d302a8d
MuhammadTaha <m.taha@ventzke-media.de>
2019-07-31 11:01:42 +0200

    dataset with updated code

--- main.py
+++ main.py
@@ -12,21 +12,16 @@ from math import sqrt
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.recurrent import LSTM
-
+import matplotlib.pyplot as plt2
 
 def get_stock_data(stock_name, normalized=0):
-    print('here')
-    # url="http://www.google.com/finance/historical?q="+stock_name+"&startdate=Jul+12%2C+2013&enddate=Jul+11%2C+2017&num=30&ei=rCtlWZGSFN3KsQHwrqWQCw&output=csv"
-
-    url="http://www.google.com/finance/historical?q=%s&ei=u-lHWfGPNNWIsgHHqIqICw&output=csv" % stock_name
 
     # col_names = ['Date','Open','High','Low','Close','Volume']
     # col_names = ['Date','Sunrise','Sunset','Daylength','HourofDay','HourNumber','Day','Day_No','NumberTripsperday','RelativeFrequency','PercentageTripperHour','Numberoftripsperhour','Day_or_Night','1_or_0']
-    col_names = ['Date', 'Sunrise', 'Sunset', 'Daylength', 'HourofDay', 'HourNumber', 'Day', 'Day_No']
+    col_names = ['Date', 'Sunrise', 'Sunset', 'Daylength', 'HourofDay', 'HourNumber', 'Day_No']
     stocks = pd.read_csv("Dataset_Daylight_Rainfall_Berlin2.csv", header=0, names=col_names)
     df = pd.DataFrame(stocks)
-    print(df)
-    df.drop(df.columns[[0,3,5]], axis=1, inplace=True)
+    df.drop(df.columns[[0,3,5,6]], axis=1, inplace=True)
     return df
 
 
@@ -49,13 +44,16 @@ df.tail()
 def load_data(stock, seq_len):
     amount_of_features = len(stock.columns)
     data = stock.as_matrix() #pd.DataFrame(stock)
-    sequence_length = seq_len + 1
-    result = []
-    for index in range(len(data) - sequence_length):
-        result.append(data[index: index + sequence_length])
+    print(data)
+    # sequence_length = seq_len + 1
+    # result = []
+    # for index in range(len(data) - sequence_length):
+    #     result.append(data[index: index + sequence_length])
 
-    result = np.array(result)
+    result = np.array(data)
+    # print(result)
     row = round(0.9 * result.shape[0])
+    print(row)
     train = result[:int(row), :]
     x_train = train[:, :-1]
     y_train = train[:, -1][:,-1]
@@ -125,14 +123,14 @@ print('Test Score: %.2f MSE (%.2f RMSE)' % (testScore[0], math.sqrt(testScore[0]
 diff=[]
 ratio=[]
 p = model.predict(X_test)
-for u in range(len(y_test)):
-    pr = p[u][0]
-    ratio.append((y_test[u]/pr)-1)
-    diff.append(abs(y_test[u]- pr))
+# for u in range(len(y_test)):
+#     pr = p[u][0]
+#     ratio.append((y_test[u]/pr)-1)
+#     diff.append(abs(y_test[u]- pr))
     #print(u, y_test[u], pr, (y_test[u]/pr)-1, abs(y_test[u]- pr))
 
 
-import matplotlib.pyplot as plt2
+
 
 plt2.plot(p,color='red', label='prediction')
 plt2.plot(y_test,color='blue', label='y_test')
